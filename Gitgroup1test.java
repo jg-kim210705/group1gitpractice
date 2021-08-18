c918998f7ad0cc6f10bb063a75764e0623cec536
DESKTOP-3JMNGE2\bitcamp <thehappypool@naver.com>
2021-08-18 20:54:05 +0900

    First commentary

---
+++ Gitgroup1test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Gitgroup1test {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 0;
+		a = sc.nextInt();
+
+		switch(a) {
+		case 1 : break;
+		case 2 : break;
+		case 3 : break;
+		case 4 : break;
+		default : break;
+		}
+
+	}
+
+}
