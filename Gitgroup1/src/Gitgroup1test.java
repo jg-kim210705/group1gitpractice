bc20cc45a57db72882d0bc205014ec2ca7821663
gombibi <87167986+gombibi@users.noreply.github.com>
2021-08-20 15:13:47 +0900

    test1

--- Gitgroup1/src/Gitgroup1test.java
+++ Gitgroup1/src/Gitgroup1test.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
 
+
+class hello{
+	String stuno;
+	int nono;
+	public String getStuno() {
+		return stuno;
+	}
+	public void setStuno(String stuno) {
+		this.stuno = stuno;
+	}
+	public int getNono() {
+		return nono;
+	}
+	public void setNono(int nono) {
+		this.nono = nono;
+	}
+	
+	
+}
+
 public class Gitgroup1test {
+	
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = 0;
 		a = sc.nextInt();
+		hiyo hiyo = new hiyo();
 
 		switch(a) {
-		case 1 : break;
+		case 1 : 
+			hiyo.ho();
+			break;
 		case 2 : break;
 		case 3 : break;
 		case 4 : break;
 		default : break;
 		}
+		
 
 	}
 
---
+++ Gitgroup1/src/hiyo.java
@@ -0,0 +1,8 @@
+
+public class hiyo {
+	
+	public void ho () {
+		System.out.println("그냥 만들어본 함수");
+	}
+
+}
