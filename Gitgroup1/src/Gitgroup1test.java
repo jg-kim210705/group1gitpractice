95a0b0851b4f73f063252799cc6a154a574faa63
YuJeong <jjeongyu531@gmail.com>
2021-08-20 16:18:16 +0900

    에러메세지삭제

--- Gitgroup1/src/Gitgroup1test.java
+++ Gitgroup1/src/Gitgroup1test.java
@@ -31,14 +31,8 @@ public class Gitgroup1test {
 	
 
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
-		System.out.println("아아아");
-		
-=======
 		System.out.println("현재 연결된 번호: 1번, 2번, 3번");
 		System.out.println("번호를 입력하세요");
->>>>>>> 9534ff88738e35d8bdb0ce48aa02b6be7a0176e4
 		Scanner sc = new Scanner(System.in);
 		
 		int a = 0;
