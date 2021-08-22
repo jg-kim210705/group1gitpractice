ae5111af9678f83cb9127b3f64ded627029bb778
subakibaksu <js01072941338@gmail.com>
2021-08-22 17:44:58 +0900

    "just now"

--- Gitgroup1/src/YOON.java
+++ Gitgroup1/src/YOON.java
@@ -2,5 +2,6 @@ public class YOON {
     public static void main(String[] args) {
         System.out.println("yoonjeogn kimm");
         System.out.println("another thing started");
+        System.out.println("new branch?");
     }
 }
