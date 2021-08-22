ae8bfbfdce41068935b01600fd3d747927bf8355
KIMJINGUN <thehappypool@naver.com>
2021-08-22 12:12:30 +0900

    last

--- Gitgroup1/src/Gitgroup1test.java
+++ Gitgroup1/src/Gitgroup1test.java
@@ -40,12 +40,13 @@ public class Gitgroup1test {
 		System.out.println("현재 연결된 번호: 1번, 2번, 3번, 6번");
 		System.out.println("번호를 입력하세요");
 		Scanner sc = new Scanner(System.in);
+
 		
 		int a = 0;
 		a = sc.nextInt();
 		hiyo hiyo = new hiyo();
 		Yu1 yu = new Yu1();
-		jingunZzang jz = new jingunZzang();
+//		jingunZzang jz = new jingunZzang();
 
 		switch(a) {
 		case 1 : 
@@ -55,7 +56,7 @@ public class Gitgroup1test {
 			Yu1.Yu();
 			break;
 		case 3 :
-			jz.jingunGanzi();
+//			jz.jingunGanzi();
 			break;
 		case 4 : break;
 		default : break;
